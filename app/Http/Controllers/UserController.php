a8837aeb98b69ce5a457a75a69d79dda51aeb582
andreasprass <andreasprasetya29@gmail.com>
2023-07-19 03:49:49 +0700

    fixing delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Ranking;
+use App\Models\Scoring;
 use App\Models\Division;
+use App\Models\Normalisasi;
 use Illuminate\Http\Request;
+use App\Models\KandidatPenilaian;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -46,15 +50,35 @@ class UserController extends Controller
     }
 
     public function delete_user($id){
-        User::destroy($id);
-        return redirect()->route('get_users')->with('success', 'The data has been deleted');
 
-        $kand_id = KandidatPenilaian::where('criteria_id',$criteria_id)->first('id');
-        Scoring::where('kriteria_penilaian',$krit_id->id)->delete();
-        Normalisasi::where('kriteria_penilaian',$krit_id->id)->delete();
-        KriteriaPenilaian::where('criteria_id',$criteria_id)->delete();
-        Criteria::where('id',$criteria_id)->delete();
-        return redirect()->route('criterias');
+        $kand = KandidatPenilaian::where('user_id',$id);
+        if($kand->exists() == true){ 
+            $alternatif = KandidatPenilaian::where('user_id',$id)->first();
+            $scoring = Scoring::where('kandidat_penilaian',$alternatif->id);  
+            $norm = Normalisasi::where('kandidat_penilaian',$alternatif->id);
+            $rank = Ranking::where('kandidat_penilaian',$alternatif->id);
+            if($scoring->exists() == true){
+                $scoring->delete();
+                if($norm->exists() == true){
+                    $norm->delete();
+                    if($rank->exists() == true){
+                        $rank->delete();
+                    }
+                }elseif($rank->exists() == true){
+                    $rank->delete();
+                }
+            }elseif($norm->exists() == true){
+                $norm->delete();
+                if($rank->exists() == true){
+                    $rank->delete();
+                }
+            }
+            $kand->delete();
+            User::where('id',$id)->delete();
+        }else{
+            User::where('id',$id)->delete();
+        }
+        return redirect()->route('get_users')->with('success', 'The data has been deleted');
     }
 
 }
